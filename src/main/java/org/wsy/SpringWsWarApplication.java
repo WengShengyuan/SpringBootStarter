1a06b8aa825c9350e3f6159ef4bac225e9c341be
wengshengyuan <wengshengyuan@gmail.com>
2016-02-15 22:16:06 +0800

    properties

--- src/main/java/org/wsy/SpringWsWarApplication.java
+++ src/main/java/org/wsy/SpringWsWarApplication.java
@@ -2,10 +2,12 @@ package org.wsy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 public class SpringWsWarApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWsWarApplication.class, args);
 	}
