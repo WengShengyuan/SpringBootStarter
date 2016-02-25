d708ca264c650116496c054312a3f4f6ce566298
6655449987 <6655449987qq@gmail.com>
2016-02-25 14:03:55 +0800

    commit test

--- src/main/java/org/wsy/spring/security/authenticationprovider/MyAuthenticationProvider.java
+++ src/main/java/org/wsy/spring/security/authenticationprovider/MyAuthenticationProvider.java
@@ -1,13 +1,23 @@
 package org.wsy.spring.security.authenticationprovider;
 
+import javax.annotation.Resource;
+
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
+import org.wsy.core.modules.user.service.SystemUserService;
+import org.wsy.core.modules.user.service.UserRoleService;
 
 @Component
 public class MyAuthenticationProvider implements AuthenticationProvider {
 
+	@Resource(name = "SystemUserServiceImpl")
+	private SystemUserService systemUserService;
+	
+	@Resource(name = "UserRoleServiceImpl")
+	private UserRoleService userRoleService;
+	
 	@Override
 	public Authentication authenticate(Authentication arg0) throws AuthenticationException {
 		// TODO Auto-generated method stub
